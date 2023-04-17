e3211608fe4f49713944db4a99e2b94284bb3129
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-04-17 23:23:51 +0530

    import module

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,1 +1,6 @@
 import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles";
+import bcrypt from "bcrypt.js"
+import JWT from "jsonwebtoken"
+import config from "../config";
+import crypto from "crypto"
