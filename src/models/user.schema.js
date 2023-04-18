9e8ac776ada8d7379f19d0a92c453c1d7829ad33
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-04-18 23:29:42 +0530

    Add user schema

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -4,3 +4,28 @@ import bcrypt from "bcrypt.js"
 import JWT from "jsonwebtoken"
 import config from "../config";
 import crypto from "crypto"
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: ["true", "Name is required"],
+        maxLength: [50, "Name must be less than 50 chars"]
+    },
+    email: {
+        type: String,
+        required: ["true", "Email is required"],   
+    },
+    password: {
+        type: String,
+        required: ["true", "Password is required"],
+        minLength: [8, "Password must be at leat 8 chars"],
+        select: false
+    },
+    role: {
+        type: String,
+        enum: Object.values(AuthRoles),
+        default: AuthRoles.USER
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date
+}, {timestamps: true})
